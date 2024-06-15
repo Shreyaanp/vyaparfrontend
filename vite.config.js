e2361fbce7b8619b2ad9d59c7a4b671b7de959e0
Shreyaanp <shreyaan.pradhan2020@vitstudeent.ac.in>
2024-06-15 09:58:09 +0545

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import history from 'connect-history-api-fallback';
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = resolve(__filename, '..');
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    middlewareMode: true,
-    setup(app) {
-      app.use(
-        history({
-          rewrites: [
-            { from: /\/.*/, to: '/' }, // Serve index.html for all routes
-          ],
-        })
-      );
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'), // Specify the location of index.html
-      },
-    },
-  },
 });
