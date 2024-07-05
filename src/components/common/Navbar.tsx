e7a0ddf5ff05246e7e087ccc376fb0ed1a42072b
Shreyaanp <shreyaan.pradhan2020@vitstudeent.ac.in>
2024-07-05 09:33:59 +0545

    live data 1 push for prod

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <nav className="bg-white shadow p-4 flex justify-between items-center">
       <div className="text-4xl font-bold text-black-600 font-ubuntu">
         Vya<span className="text-[#FCBD01]">par</span> Launch
         <span className="text-[#FCBD01]">pad</span>
       </div>
-      <button className="px-4 py-2 bg-gray-200 rounded-full text-gray-700">
+      <button 
+      onClick={() => navigate("/")}
+      className="px-4 py-2 bg-gray-200 rounded-full text-gray-700">
         Exit
       </button>
     </nav>
--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,12 +1,11 @@
-import React, { useState, ReactNode } from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "../components/common/Header";
 import Sidebar from "../components/Sidebar/index";
 
 const DefaultLayout = ({ children }) => {
   const { pathname } = useLocation();
-  const sidebarOpen = false; // Assuming you have a state for this
-  const setSidebarOpen = () => {}; // Assuming you have a method for this
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   // Define the routes that should only render children
   const simpleRoutes = [
@@ -15,20 +14,23 @@ const DefaultLayout = ({ children }) => {
     "/product-page",
     "/product-onboarding",
     "/signin",
-    "/",
-    "/live"
+    "/"
   ];
 
-  if (simpleRoutes.includes(pathname)) {
+  const isLiveRoute = pathname.startsWith("/live");
+
+  if (simpleRoutes.includes(pathname) || isLiveRoute) {
     return <>{children}</>;
   }
 
   return (
     <div className="dark:bg-boxdark-2 dark:text-bodydark bg-[#FDE7A8]">
       <div className="flex h-screen overflow-hidden">
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        {!isLiveRoute && (
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        )}
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          <Header />
+          {!isLiveRoute && <Header />}
           <main>
             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
               {children}
